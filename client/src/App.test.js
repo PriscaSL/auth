89fb325687730c6e691f07ddf05fd74625800249
prisca <prisca@localhost.localdomain>
2024-10-07 21:09:11 +0300

    change dev successs

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import App from './App'; // Chemin vers votre composant App
+import App from './App';
+import '@testing-library/jest-dom';
 
 test('renders login heading', () => {
   render(
