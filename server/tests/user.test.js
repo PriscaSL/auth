ff21513a84ec7c86cf86b6ccd2bd5aac234cab74
prisca <prisca@localhost.localdomain>
2024-10-07 20:49:48 +0300

    change dev succe

---
+++ server/tests/user.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const app = require('../index.js'); // Assurez-vous que le chemin vers votre fichier app.js est correct
+
+describe('GET /api/users', () => {
+  it('should return a list of users', async () => {
+    const res = await request(app).get('/api/users');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toBeInstanceOf(Array);
+  });
+});
+8
