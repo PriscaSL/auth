b5030440f46bdb710714e581f9c570fff5dabbd7
prisca <prisca@localhost.localdomain>
2024-10-08 08:05:08 +0300

    new test

--- server/db.js
+++ server/db.js
@@ -1,36 +1,17 @@
 const mongoose = require("mongoose");
 
-let isConnected;
+module.exports = () => {
+	const connectionParams = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	};
 
-const connectToDatabase = async () => {
-    const connectionParams = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    };
 
-    mongoose.set('strictQuery', true);
+	mongoose.set('strictQuery', true);
 
-    if (isConnected) {
-        console.log("Déjà connecté à la base de données.");
-        return;
-    }
-
-    try {
-        console.log("Tentative de connexion à la base de données...");
-        await mongoose.connect(process.env.DB, connectionParams);
-        isConnected = true;
-        console.log("Connecté à la base de données avec succès");
-    } catch (error) {
-        console.error("Erreur lors de la connexion à la base de données!", error);
-    }
+	mongoose.connect(process.env.DB, connectionParams)
+		.then(() => console.log("Connected to database successfully"))
+		.catch((error) => {
+			console.error("Could not connect to database!", error);
+		});
 };
-
-const disconnectFromDatabase = async () => {
-    if (isConnected) {
-        await mongoose.connection.close();
-        isConnected = false;
-        console.log("Déconnecté de la base de données");
-    }
-};
-
-module.exports = { connectToDatabase, disconnectFromDatabase };
